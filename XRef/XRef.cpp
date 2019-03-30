9855e4d95677df746ee114596933f00fcb512455
jarquafelmu <thechaoselement@gmail.com>
2019-03-30 17:04:03 -0600

    error with using string in lambda

--- XRef/XRef.cpp
+++ XRef/XRef.cpp
@@ -1,5 +1,8 @@
 #include "XRef.h"
+#include <algorithm>
+#include <regex>
 
+using namespace std;
 
 XRef::XRef () {}
 
@@ -7,20 +10,39 @@ XRef::XRef () {}
 XRef::~XRef () {}
 
 void XRef::process( std::vector<std::string> const& data ) {
-  
+  regex pattern ( R"(\b[A-Za-z\-']\b)" );
+  size_t lineno {1};
+  for (auto const& item : data ) {
+    auto begin = sregex_iterator ( item.begin (), item.end (), pattern );
+    auto end = sregex_iterator ();
+    for_each ( begin, end, [&]( string& word ) { this->logWord ( word, lineno ); } );
+    ++lineno;
+  }
 }
 
-void XRef::note( std::string const& word, size_t const lineno ) {
-  auto entry = reference_.find ( word );
-  if (entry != reference_.end ()) {
-    // second is a map, need to see if lineno is there, if so increment the value
-    // if not insert a new start
-    auto const entry_val = entry->second.find ( lineno );
-    if (entry_val == entry->second.end ()) {
-      entry->second [lineno] = default_count_;
-    }
-    else {
-      entry_val->second++;
-    }
+void XRef::logWord( std::string const& word, size_t const lineno ) {
+   auto entry = reference_.find ( word );
+   if (entry != reference_.end ()) {
+     //handleEntry ( entry, lineno );
+   } else {
+     createEntry ( word, lineno );
+   }
+}
+
+void XRef::createEntry(std::string const& word, size_t const lineno) {
+  //map<size_t, size_t> m;
+  //m [lineno] = default_count_;
+  reference_ [word] [lineno] = default_count_;
+}
+
+void XRef::handleEntry( std::map<string, map<size_t, size_t>>::iterator it, size_t const& lineno ) const {
+  // second is a map, need to see if lineno is there, if so increment the value
+  // if not insert a new start
+  auto const entry_val = it->second.find ( lineno );
+  if ( entry_val == it->second.end () ) {
+    it->second [lineno] = default_count_;
+  }
+  else {
+    entry_val->second++;
   }
 }
--- XRef/XRef.h
+++ XRef/XRef.h
@@ -6,10 +6,12 @@
 class XRef {
   size_t default_count_ = 1;
   std::map<std::string, std::map<size_t, size_t>> reference_ {};
+  void logWord ( std::string const& word, size_t const lineno );
+  void createEntry ( std::string const& word, size_t const lineno );
+  void handleEntry ( std::map<std::string, std::map<unsigned, unsigned>>::iterator it, size_t const& lineno ) const;
 public:
   XRef ();
   ~XRef ();
   void process( std::vector<std::string> const& data );
-  void note ( std::string const& word, size_t const lineno );
 };
 
